d68b031fb3b97cce36f815da617ff0e35c9bc5c9
Ilya Ivanov <static.ila@gmail.com>
2015-08-11 13:44:05 +0300

    Finished with collapse menu design

--- collapse-menu/collapse-menu.js
+++ collapse-menu/collapse-menu.js
@@ -1,11 +1,14 @@
 $(document).on("click", "a[data-collapse-target]", function () {
-    var v = $(this).attr('data-collapse-target');
-    var destinationElement = $('[data-collapse-menu="' + v + '"]');
+    var sourceElement = $(this).attr('data-collapse-target');
+
+    var destinationElement = $('[data-collapse-menu="' + sourceElement + '"]');
+
     var destinationElementGroup = destinationElement.attr('data-collapse-group');
 
     var openElementsInGroup = $('.in[data-collapse-group="' + destinationElementGroup + '"]');
 
-    openElementsInGroup.collapse('hide');
+    if(openElementsInGroup.length > 0)
+        openElementsInGroup.collapse('hide');
 
     destinationElement.collapse('toggle');
 });
