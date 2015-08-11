175c9e63991496955daf60a5fe79bf603d27bf96
Ilya Ivanov <static.ila@gmail.com>
2015-08-11 13:46:43 +0300

    Wrapped plugin into IIFE

--- collapse-menu/collapse-menu.js
+++ collapse-menu/collapse-menu.js
@@ -1,14 +1,16 @@
-$(document).on("click", "a[data-collapse-target]", function () {
-    var sourceElement = $(this).attr('data-collapse-target');
+(function ($){
+    $(document).on("click", "a[data-collapse-target]", function () {
+        var sourceElement = $(this).attr('data-collapse-target');
 
-    var destinationElement = $('[data-collapse-menu="' + sourceElement + '"]');
+        var destinationElement = $('[data-collapse-menu="' + sourceElement + '"]');
 
-    var destinationElementGroup = destinationElement.attr('data-collapse-group');
+        var destinationElementGroup = destinationElement.attr('data-collapse-group');
 
-    var openElementsInGroup = $('.in[data-collapse-group="' + destinationElementGroup + '"]');
+        var openElementsInGroup = $('.in[data-collapse-group="' + destinationElementGroup + '"]');
 
-    if(openElementsInGroup.length > 0)
-        openElementsInGroup.collapse('hide');
+        if(openElementsInGroup.length > 0)
+            openElementsInGroup.collapse('hide');
 
-    destinationElement.collapse('toggle');
-});
+        destinationElement.collapse('toggle');
+    });
+})(jQuery);
